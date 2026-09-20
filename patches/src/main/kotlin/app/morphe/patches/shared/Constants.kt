5e5b184361e4bee2ca7772ab01de7f058cf96b98
HellLord <ratul.debnath.year@gmail.com>
2026-09-20 21:01:48 +0600

    chore: Add supported bongo versions

--- patches/src/main/kotlin/app/morphe/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/morphe/patches/shared/Constants.kt
@@ -12,7 +12,15 @@ object Constants {
         appIconColor = 0XD8062D,
         signatures = setOf("29ccbd820d40b90e7ab5206b20bcafa538508f842d21890aca39df9ee5aec9c6"),
         targets = listOf(
-            AppTarget(version = "6.9.2", versionCode = 210060902, minSdk = 21)
+            AppTarget(version = "6.10.3", versionCode = 210061003, minSdk = 21),
+            AppTarget(version = "6.10.2", versionCode = 210061002, minSdk = 21),
+            AppTarget(version = "6.10.0", versionCode = 210061000, minSdk = 21),
+            AppTarget(version = "6.9.2", versionCode = 210060902, minSdk = 21),
+            AppTarget(version = "6.9.1", versionCode = 210060901, minSdk = 21),
+            AppTarget(version = "6.9.0", versionCode = 210060900, minSdk = 21),
+            AppTarget(version = "6.8.9", versionCode = 210060809, minSdk = 21),
+            AppTarget(version = "6.8.8", versionCode = 210060808, minSdk = 21),
+            AppTarget(version = "6.8.6", versionCode = 210060806, minSdk = 21)
         )
     )
 
